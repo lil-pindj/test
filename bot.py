aaa9376f8df85ebd30275463d81d0a2282e4a52f
lil-pindj <nastyha2yha@mail.ru>
2024-09-17 16:53:09 +0300

    reformat file

--- bot.py
+++ bot.py
@@ -3,18 +3,22 @@ from telebot.types import Message
 import s_taper
 import create_db
 from config import token
+
 bot = telebot.TeleBot(token)
 items = {}
 
+
 @bot.message_handler(commands=['start'])
-def start_bot(message:Message):
+def start_bot(message: Message):
     clients = create_db.clients.read_all()
     for client in clients:
         if message.chat.id == client[1]:
-            bot.send_message(message.chat.id, text = f'Здравствуйте, {client[0]}')
+            bot.send_message(message.chat.id, text=f'Здравствуйте, {client[0]}')
             return
     bot.send_message(message.chat.id, text='Введите свой ник')
     bot.register_next_step_handler(message, registration)
+
+
 def registration(message):
     answer = message.text
     create_db.clients.write([answer, message.chat.id, items])
