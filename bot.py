c07b84a220a42123254154db071167efacd6dd55
lil-pindj <nastyha2yha@mail.ru>
2024-09-17 16:39:59 +0300

    initial commit

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ import create_db
 from config import token
 bot = telebot.TeleBot(token)
 items = {}
+
 @bot.message_handler(commands=['start'])
 def start_bot(message:Message):
     clients = create_db.clients.read_all()
@@ -18,5 +19,6 @@ def registration(message):
     answer = message.text
     create_db.clients.write([answer, message.chat.id, items])
     bot.send_message(chat_id=message.chat.id, text='Пользователь добавлен')
+
 
 bot.infinity_polling()
